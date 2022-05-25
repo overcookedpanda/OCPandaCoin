310d7e6138a069f021583ee0e3d9297384562ee5
Mr-Leshiy <leshiy12345678@gmail.com>
2022-05-25 12:22:00 +0300

    add rpc_work_exceeded.py functional test

---
+++ test/functional/rpc_work_exceeded.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-2019 The Bitcoin Core developers
+# Copyright (c) 2019-2021 Xenios SEZC
+# https://www.veriblock.org
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from decimal import Decimal
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    connect_nodes,
+    p2p_port
+)
+
+class WorkExceededTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = True
+        self.extra_args = [[],[]]
+
+    def getblocktemplate(self):
+        arg = {
+            "capabilities": ["coinbasetxn", "workid", "coinbase/append"],
+            "rules": ["segwit"]
+        }
+        return self.nodes[0].getblocktemplate(arg)
+
+    def run_test(self):
+        # spam with the getblock template rpc function
+        for i in range(100000):
+            res = self.getblocktemplate()
+            print(res)
+
+
+        assert False
+
+
+
+if __name__ == '__main__':
+    WorkExceededTest().main()
